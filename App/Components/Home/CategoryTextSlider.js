a705211b1c9e9d8854f2607d27332bd5d8b8fe8d
Essam Al Deen <essamalshemriy@gmail.com>
2024-03-03 21:45:52 +0300

    Add: categories

--- App/Components/Home/CategoryTextSlider.js
+++ App/Components/Home/CategoryTextSlider.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-function CategoryTextSlider() {
+import React from 'react';
+import { FlatList, Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import BrandColors from '../../Shared/BrandColors';
 
+function CategoryTextSlider() {
+  
+  const DATA  = [
+    { id: 1, name: 'Politics' },
+    { id: 2, name: 'Technology' },
+    { id: 3, name: 'Entertainment' },
+    { id: 4, name: 'Sports' },
+    { id: 5, name: 'International' },
+    { id: 6, name: 'Local' }
+  ];
+
+ 
     
   return (
     <View>
-        <Text>Categories</Text>
+        <FlatList
+        data={DATA}
+        horizontal={true}
+        renderItem={({item}) => (
+          <TouchableOpacity style={styles.item}>
+            <Text style={styles.title}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.id.toString()}
+        />
     </View>
   )
 }
+const styles = StyleSheet.create({
+  item: {
+    padding: 8,
+    marginVertical: 8,
+    marginHorizontal: 5,
+    backgroundColor: BrandColors.lightGray,
+    opacity: 0.7,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 16,
+  },
+});
 
-export default CategoryTextSlider
+export default CategoryTextSlider;
